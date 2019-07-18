d65df3a4ba4f19adfec33802bb08a96b6cb7b1e9
Dominique Berton <dominique.berton@gmail.com>
2019-07-18 14:25:21 -0400

    Added unit tests

--- src/app/table-paging/table-paging.component.spec.ts
+++ src/app/table-paging/table-paging.component.spec.ts
@@ -1,25 +1,70 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TablePagingComponent } from './table-paging.component';
+import { UrlManagerService } from '../service/url-manager.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('TablePagingComponent', () => {
   let component: TablePagingComponent;
   let fixture: ComponentFixture<TablePagingComponent>;
+  let urlManager: UrlManagerService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TablePagingComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule,
+      ],
+      declarations: [TablePagingComponent],
+      providers: [UrlManagerService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TablePagingComponent);
     component = fixture.componentInstance;
+    urlManager = TestBed.get(UrlManagerService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('ngOnInit', () => {
+    component.ngOnInit();
+    expect(component.data).toBeTruthy();
+    expect(component.data.length).toEqual(200);
+    expect(component.numRows).toEqual(10);
+    expect(component.pagedData).toBeTruthy();
+    expect(component.pagedData.length).toEqual(10);
+  });
+
+  it('fSubmit', () => {
+    spyOn(urlManager, 'fPost').and.callThrough();
+    const myObj = { id: 1, status: 'dom-ok' };
+    component.fSubmit(myObj);
+    expect(urlManager.fPost).toHaveBeenCalledWith(myObj);
+  });
+
+  it('fPage 0', () => {
+    component.fPage(0);
+    expect(component.page).toEqual(1);
+  });
+
+  it('fPage 1000', () => {
+    component.fPage(1000);
+    expect(component.page).toEqual(20);
+  });
+
+  it('fPage 2', () => {
+    component.fPage(2);
+    expect(component.page).toEqual(2);
+    expect(component.pagedData.length).toEqual(10);
+    expect(component.pagedData[0].id).toEqual(11);
+  });
+
 });
--- src/app/table-scroll/table-scroll.component.spec.ts
+++ src/app/table-scroll/table-scroll.component.spec.ts
@@ -1,25 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TableScrollComponent } from './table-scroll.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { UrlManagerService } from '../service/url-manager.service';
 
 describe('TableScrollComponent', () => {
   let component: TableScrollComponent;
   let fixture: ComponentFixture<TableScrollComponent>;
+  let urlManager: UrlManagerService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableScrollComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule,
+      ],
+      declarations: [TableScrollComponent],
+      providers: [UrlManagerService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TableScrollComponent);
     component = fixture.componentInstance;
+    urlManager = TestBed.get(UrlManagerService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit', () => {
+    component.ngOnInit();
+    expect(component.data).toBeTruthy();
+    expect(component.data.length).toEqual(200);
+    expect(component.numRows).toEqual(10);
+    expect(component.scrolledData).toBeTruthy();
+    expect(component.scrolledData.length).toEqual(10);
+  });
+
+  it('fSubmit', () => {
+    spyOn(urlManager, 'fPost').and.callThrough();
+    const myObj = { id: 1, status: 'dom-good' };
+    component.fSubmit(myObj);
+    expect(urlManager.fPost).toHaveBeenCalledWith(myObj);
+  });
+
+  it('fScroll', () => {
+    component.fScroll();
+    expect(component.scrolledData).toBeTruthy();
+    expect(component.scrolledData.length).toEqual(10);
   });
 });
